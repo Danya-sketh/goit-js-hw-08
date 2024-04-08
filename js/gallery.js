21676126f94b72da857e287a0df37fd99d303bdd
Danya-sketh <yannitc7@gmail.com>
2024-04-08 12:30:03 +0300

    fix mistakes

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,130 @@
 // import * as basicLightbox from 'basiclightbox'
 
 
+// const images = [
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+//     description: 'Hokkaido Flower',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+//     description: 'Container Haulage Freight',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+//     description: 'Aerial Beach View',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+//     description: 'Flower Blooms',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+//     description: 'Alpine Mountains',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+//     description: 'Mountain Lake Sailing',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+//     description: 'Alpine Spring Meadows',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+//     description: 'Nature Landscape',
+//   },
+//   {
+//     preview:
+//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+//     description: 'Lighthouse Coast Sea',
+//   },
+// ];
+
+
+
+
+// const container = document.querySelector('.gallery');
+
+// // add cards into site
+
+// container.innerHTML = createGallery(images);
+
+// // modal window
+
+// container.addEventListener("click", handleModalOpen)
+
+// function handleModalOpen(event){
+//   if (event.curentTarget === event.target) return // перевірка якщо не клікнемо на карточку
+
+//   const currentImage = event.target.closeset(".gallery-item")
+
+//   const imageId = Number(currentImage.dataset.id);
+
+//   const image = images.find(({id}) => id === imageId);
+
+
+
+//   const instance = basicLightbox.create(`
+// 	<div class="modal">
+//     <a class="gallery-link" href="${image.original}">
+//       <img
+//         class="gallery-image"
+//         src="${image.preview}"
+//         data-source="${image.original}"
+//         alt="${image.description}"
+//       />
+//     </a>
+//   </div>
+// `)
+
+// instance.show()
+// }
+
+
+
+
+// function createGallery(arr){
+//   return arr.map(({preview, original, description}) =>
+//   `
+//   <li class="gallery-item">
+//     <a class="gallery-link" href="${original}">
+//       <img
+//         class="gallery-image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//       />
+//     </a>
+//   </li>`).join("")
+// }
+
 const images = [
   {
     preview:
@@ -68,63 +192,40 @@ const images = [
     description: 'Lighthouse Coast Sea',
   },
 ];
-
-
-
-
 const container = document.querySelector('.gallery');
-
-// add cards into site
-
 container.innerHTML = createGallery(images);
-
-// modal window
-
 container.addEventListener("click", handleModalOpen)
-
 function handleModalOpen(event){
-  if (event.curentTarget === event.target) return // перевірка якщо не клікнемо на карточку
-
-  const currentImage = event.target.closeset(".gallery-item")
-
+  event.preventDefault();
+  if (event.currentTarget === event.target) return;
+  const currentImage = event.target.closest(".gallery-item");
   const imageId = Number(currentImage.dataset.id);
-
-  const image = images.find(({id}) => id === imageId);
-
-
-
+  const image = images.find(({preview, original, description}) => preview === currentImage.querySelector('.gallery-image').src);
   const instance = basicLightbox.create(`
-	<div class="modal">
-    <a class="gallery-link" href="${image.original}">
+  <div class="modal">
       <img
         class="gallery-image"
         src="${image.preview}"
         data-source="${image.original}"
         alt="${image.description}"
       />
-    </a>
   </div>
-`)
-
-instance.show()
+`);
+  instance.show();
+  const modalImage = instance.element().querySelector('.gallery-image');
+  modalImage.addEventListener('click', () => {
+    instance.close();
+  });
 }
-
-
-
-
 function createGallery(arr){
   return arr.map(({preview, original, description}) =>
   `
   <li class="gallery-item">
-    <a class="gallery-link" href="${original}">
-      <img
-        class="gallery-image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
   </li>`).join("")
 }
-
-// container.insertAdjacentHTML("beforeend", createGallery(gallery));
